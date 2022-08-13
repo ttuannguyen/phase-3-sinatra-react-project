867f86376ea54da487978a57dbeb60d49e1b5ee1
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-08-13 10:34:00 -0400

    built out the post method in vendors controller to create new vendors

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -2,10 +2,8 @@ class CuisinesController < ApplicationController
 
     get "/cuisines" do
         { message: "Here are all cuisines!" }.to_json
-      end
-    
-    get "/cuisines/:id" do
-        cuisine = Cuisine.find(params[:id])
     end
+
+    # need a post request 
       
 end
--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -3,17 +3,36 @@ require 'pry'
 class VendorsController < ApplicationController
 
     get "/vendors" do
-        #
+        vendors = Vendor.all
+        vendors.to_json
     end
 
-    get "/vendors:id" do
+    # get "/vendors/:id" do
+    #     vendor = Vendor.find(params[:id]) 
+    #     vendor.to_json
+    # end
+
+    post "/vendors" do 
+        # create a new review in the database
+        # params is a hash of key-value pairs coming from the body of the request
+        vendor = Vendor.create(
+            name: params[:name],
+            booth_number: params[:booth_number],
+            cuisine_id: params[:cuisine_id],
+            comment: params[:comment] 
+        ) 
+        # send back a response with the created review as JSON
+        vendor.to_json
+    end
+    # collection.create for associated objects
+
+    patch "/vendors/:id" do
     end
 
-    post "vendors" do 
+    delete "/vendors/:id" do
     end
 
-    delete "/vendors:id" do
-    end
+    #find the cuisine id and make the collection using the collection method
 
 
 end
