1a7c9ae0b3f6ff58324f076d1d1c55c36894d8f4
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-08-15 15:22:49 -0400

    add a get method in Cuisines controller to send all cuisines to the client side

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -2,8 +2,12 @@ class CuisinesController < ApplicationController
 
     get "/cuisines" do
         { message: "Here are all cuisines!" }.to_json
+        cuisines = Cuisine.all
+        cuisines.to_json
     end
 
     # need a post request 
+
+
       
 end
